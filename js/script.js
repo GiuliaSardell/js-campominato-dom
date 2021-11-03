96eef3d6e091e08f84cfc7cea5c74781dee8f4f7
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-03 11:52:05 +0100

    cambio colore cella al click

--- js/script.js
+++ js/script.js
@@ -20,8 +20,6 @@ function play(){
   const gridLevels = [100,81,49];
   const cellNumbers = gridLevels[level-1];
   const cellRow = Math.sqrt(cellNumbers);
-  // const bombs = generateBombs();
-  // const BOMBS_NUMBER = 16;
 
   console.log('numero per dividere le celle: ',cellRow)
   console.log('value selezionato: ',level)
@@ -56,7 +54,6 @@ function play(){
       cell.style.width = cellSize;
       cell.style.height = cellSize;
 
-      // cell.addEventListener('click', handleClickCell);
 
       grid.append(cell)
       
@@ -64,71 +61,8 @@ function play(){
 
     }
 
-    
 
-    
-
-    
   }
-
 
 }
 
-// level.options
-
-// if(level == level.options[0]){
-//   let value = level.options[0].value
-//   let numSquare = 100
-//   console.log('value',value)
-//   console.log('numSquare', numSquare)
-// } else if (level == level.options[1]){
-//   let value = level.options[1].value
-//   let numSquare = 81
-//   console.log('value',value)
-//   console.log('numSquare', numSquare)
-// } else if (level == level.options[2]){
-//   let value = level.options[2].value
-//   let numSquare = 49
-//   console.log('value',value)
-//   console.log('numSquare', numSquare)
-// }
-
-
-// if(level.value == "1"){
-//   console.log('funziona')
-//   const squareNum = 100;
-//   console.log(squareNum)
-// } else if (level.value == "2"){
-//   console.log('funziona2')
-//   const squareNum = 81;
-//   console.log(squareNum)
-// }
-
-
-
-// for (let i = 0; i < 100; i++){
-//   const sq  = createSquare(container);
-
-//   sq.addEventListener('click', function(){
-//     this.classList.add('clicked')
-//   })
-// }
-
-// function createSquare(target){
-//   const sq = document.createElement('div');
-//   // const uniqueNumber = uniqueRandom (listNumber, 1, 100)
-//   // sq.innerHTML= uniqueNumber;
-//   sq.className='square';
-//   target.append(sq)
-//   return sq
-// }
-
-// function uniqueRandom (list, min, max){
-//   let number = generateRandomNum (min,max)
-//   console.log(number)
-// }
-
-// function generateRandomNum (min, max){
-//   return Math.floor(Math.random()*max-min +1)+min
-//   // listNumber.push(number)
-// }
